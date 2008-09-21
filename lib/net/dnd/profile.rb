6b1437c863ce312839154eb92136b38bfaecc132
Brian V. Hughes <brianvh@dartmouth.edu>
2008-09-21 05:17:20 +0000

    =Refactored out a return_field method in the Profile class for returning the value of a named field. Replaced duplicate code in the [] method and in method_missing.

--- lib/net/dnd/profile.rb
+++ lib/net/dnd/profile.rb
@@ -1,6 +1,6 @@
 
 module Net ; module DND
-  
+
   class Profile
 
     def initialize(fields, items)
@@ -17,25 +17,23 @@ module Net ; module DND
     end
 
     def [](field)
-      field = field.to_sym
-      return @attributes[field] if @attributes.has_key?(field)
-      raise "Field #{field} does not exist in this Profile"
+      return_field(field)
     end
 
     private
     
     def method_missing(method_id)
       attrib_name = method_id.to_s
-      if @attributes.has_key?(method_id)
-        @attributes[method_id]
-      elsif attrib_name[-1, 1] == '?'
-        attrib_name = attrib_name.chop.to_sym
-        @attributes.has_key?(attrib_name)
-      else
-        raise "Field #{method_id} does not exist in this Profile"
-      end
+      return @attributes.has_key?(attrib_name.chop.to_sym) if attrib_name[-1, 1] == '?'
+      return_field(method_id)
+    end
+
+    def return_field(field)
+      field = field.to_sym
+      return @attributes[field] if @attributes.has_key?(field)
+      raise "Field #{field} does not exist in this Profile"
     end
 
   end
-  
+
 end; end
