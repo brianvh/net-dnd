7882c1863af62d3a589f62a6c920212cbd79dcae
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-03 11:35:01 -0500

    Removed files used by the old gem packaging system.

--- stories/multiple_find.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-require File.dirname(__FILE__) + '/helper'
-
-with_steps_for :multiple_find do
-  run_story_file("multiple_find")
-end
--- stories/single_find.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-require File.dirname(__FILE__) + '/helper'
-
-with_steps_for :single_find do
-  run_story_file("single_find")
-end
